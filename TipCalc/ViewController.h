437b29c0127f12502c09f79abcac14c7451e3bd7
erabug <erabug@gmail.com>
2015-01-19 10:25:06 -0800

    Initial Commit

---
+++ TipCalc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalc
+//
+//  Created by Erika Arnold on 1/19/15.
+//  Copyright (c) 2015 Erika Arnold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
